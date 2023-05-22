b52198c0d7a634a23c85b5953cb0827bf12a4798
epolga <olga.epstein@gmail.com>
2023-05-22 15:50:22 +0300

    Remove redundant code

--- GitHubSearch/API/GitHubController.cs
+++ GitHubSearch/API/GitHubController.cs
@@ -10,44 +10,6 @@ namespace GitHubSearch.API
     [ApiController]
     public class GitHubController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> GetReps()
-        {
-            var client = new GitHubClient(new ProductHeaderValue("Olga"));
-            var tokenAuth = new Credentials("token", AuthenticationType.Anonymous);
-            client.Credentials = tokenAuth;
-            var url = "https://github.com/search?q=angular&type=repositories";
-            url = "https://api.github.com/repositories";
-            var credentials = new Credentials("epolga", "cueryuc3141", AuthenticationType.Basic);
-            var res = await client.Search.SearchRepo(new SearchRepositoriesRequest("angular university"));
-            var t = res.GetType();
-            var repo = res.Items.FirstOrDefault();
-            if (res == null)
-            {
-                return NotFound();
-            }
-            return Ok(res);
-        }
-
-        [HttpPost]
-         [Route("{name}")]
-        public async Task<IActionResult> GetReps(string name)
-        {
-            var client = new GitHubClient(new ProductHeaderValue("Olga"));
-           
-            var url = "https://github.com/search?q=angular&type=repositories";
-            url = "https://api.github.com/repositories";
-            var credentials = new Credentials("epolga", "*******", AuthenticationType.Basic);
-            var res = await client.Search.SearchRepo(new SearchRepositoriesRequest("angular university"));
-            var t = res.GetType();
-            var repo = res.Items.FirstOrDefault();
-            if (res == null)
-            {
-                return NotFound();
-            }
-            return Ok(repo);
-        }
-
         public async Task<IActionResult> Index(string searchString)
         {
             
